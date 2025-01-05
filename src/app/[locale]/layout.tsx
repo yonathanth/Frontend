b1b30dece3ac76549c257b31279b0a4e71137c26
yonathanth <yonathantenaye@gmail.com>
2025-01-06 01:26:27 +0300

    favicon should be working now

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -26,6 +26,11 @@ export default async function RootLayout({
   const messages = await getMessages();
   return (
     <html lang="en">
+      <head>
+        {/* Add your favicon here */}
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={`font-jost ${inter.className}`}>
         <ServiceProvider>
           <CartProvider>
