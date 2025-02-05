dae11c6ca860a7c25c0463efb25b47fa5f65005c
yonathanth <yonathantenaye@gmail.com>
2025-02-05 15:41:03 +0300

    message edited

--- src/app/[locale]/admin/gym-member/page.tsx
+++ src/app/[locale]/admin/gym-member/page.tsx
@@ -694,7 +694,7 @@ const GymMembersList = () => {
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-black p-6 rounded-lg w-80">
             <h3 className="text-lg font-semibold mb-4">
-              Select Freeze Duration Date for{" "}
+              Select Freeze Duration (in days) for{" "}
               {selectedMemberForFreeze?.fullName}
             </h3>
             <input
